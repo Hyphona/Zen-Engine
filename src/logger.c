04799ae8c0d3201429a6d1237cf655d17ab3bfd5
Hyphona <hyphona@gmail.com>
2026-02-14 13:15:05 +0100

    Changes to logger

--- src/logger.c
+++ src/logger.c
@@ -6,7 +6,7 @@
 /*   By: Hyphona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 12:39:00 by Hyphona           #+#    #+#             */
-/*   Updated: 2026/02/14 12:44:58 by Hyphona          ###   ########.fr       */
+/*   Updated: 2026/02/14 13:06:05 by Hyphona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /**
  * Log the string msg with prefix to the console
  */
-static size_t	log_to_console(const char *prefix, const char *msg)
+static size_t	to_console(const char *prefix, const char *msg)
 {
 	size_t	i;
 	char	*s;
@@ -26,7 +26,7 @@ static size_t	log_to_console(const char *prefix, const char *msg)
 		s = ft_strjoin(prefix, msg);
 	if (!s)
 	{
-		write(0, "logger -> strjoin() error", 25);
+		write(0, "logger.c -> ft_strjoin() error", 30);
 		return (0);
 	}
 	i = write(0, s, strlen(s));
@@ -35,7 +35,7 @@ static size_t	log_to_console(const char *prefix, const char *msg)
 }
 
 /**
- * Zen Engine logging method (to console)
+ * Zen Engine logging method
  *
  * @param mode info (0), warning (1), error (2)
  * @param msg The message to log
@@ -44,43 +44,36 @@ static size_t	log_to_console(const char *prefix, const char *msg)
  */
 size_t	zen_log(size_t mode, char *msg)
 {
-	char	*prefix;
-
-	prefix = "ZEN ENGINE -> Info: ";
 	if (mode == 0)
-		return (log_to_console(prefix, msg));
-	prefix = "ZEN ENGINE -> Warning: ";
-	if (mode == 1)
-		return (log_to_console(prefix, msg));
-	prefix = "ZEN ENGINE -> Error: ";
-	if (mode == 2)
-		return (log_to_console(prefix, msg));
-	prefix = "ZEN ENGINE -> Warning: ";
-	log_to_console(prefix, "zen_log() called with an invalid mode");
-	log_to_console(prefix, "Available modes: info (0), warning (1), error (2)");
-	log_to_console(prefix, "Fallback to warning (1) mode");
-	return (log_to_console(prefix, msg));
+		return (to_console("ZEN ENGINE->I ", msg));
+	else if (mode == 1)
+		return (to_console("ZEN ENGINE->W ", msg));
+	else if (mode == 2)
+		return (to_console("ZEN ENGINE->E ", msg));
+	to_console("ZEN ENGINE->W ", "zen_log() - Unknow mode");
+	to_console("ZEN ENGINE->W ", "Modes: info (0), warning (1), error (2)");
+	to_console("ZEN ENGINE->W ", "Fallback to warning (1)");
+	return (to_console("ZEN ENGINE->W ", msg));
 }
 
 /**
- * Same as zen_log() but for the game related stuff
+ * Game logging method
+ *
+ * @param mode info (0), warning (1), error (2)
+ * @param msg The message to log
+ * @returns On success, the number of bytes written
+ * @returns On error, 0
  */
 size_t	game_log(size_t mode, char *msg)
 {
-	char	*prefix;
-
-	prefix = "GAME -> Info: ";
 	if (mode == 0)
-		return (log_to_console(prefix, msg));
-	prefix = "GAME -> Warning: ";
-	if (mode == 1)
-		return (log_to_console(prefix, msg));
-	prefix = "GAME -> Error: ";
-	if (mode == 2)
-		return (log_to_console(prefix, msg));
-	prefix = "GAME -> Warning: ";
-	log_to_console(prefix, "game_log() called with an invalid mode");
-	log_to_console(prefix, "Available modes: info (0), warning (1), error (2)");
-	log_to_console(prefix, "Fallback to warning (1) mode");
-	return (log_to_console(prefix, msg));
+		return (to_console("GAME->I: ", msg));
+	else if (mode == 1)
+		return (to_console("GAME->W: ", msg));
+	else if (mode == 2)
+		return (to_console("GAME->E: ", msg));
+	to_console("GAME->W: ", "game_log() - Unknow mode");
+	to_console("GAME->W: ", "Modes: info (0), warning (1), error (2)");
+	to_console("GAME->W: ", "Fallback to warning (1)");
+	return (to_console("GAME->W: ", msg));
 }
