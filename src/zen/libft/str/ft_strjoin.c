4dc87057244866858fd520144a6a52bd0787cb00
Hyphona <hyphona@gmail.com>
2026-02-18 18:05:10 +0100

    ft_strjoin() optimization

--- src/zen/libft/str/ft_strjoin.c
+++ src/zen/libft/str/ft_strjoin.c
@@ -6,36 +6,12 @@
 /*   By: Hyphona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 12:39:33 by Hyphona           #+#    #+#             */
-/*   Updated: 2026/02/14 14:13:35 by Hyphona          ###   ########.fr       */
+/*   Updated: 2026/02/18 18:04:22 by Hyphona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
-static char	*ft_join(char *str, const char *s1, const char *s2)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i])
-	{
-		str[j] = s1[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (s2[i])
-	{
-		str[j] = s2[i];
-		i++;
-		j++;
-	}
-	return (str);
-}
 
 /**
  * Join two strings
@@ -43,16 +19,22 @@ static char	*ft_join(char *str, const char *s1, const char *s2)
  * @param s1 First string to join
  * @param s2 Second string to join
  * @returns On success, a pointer to the joined string
- * @returns On error, NULL
+ * @returns On fail, an empty pointer
  */
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	str = calloc(strlen(s1) + strlen(s2) + 1, sizeof(char));
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	str = calloc(s1_len + s2_len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	return (ft_join(str, s1, s2));
+	memcpy(str, s1, s1_len);
+	memcpy(str + s1_len, s2, s2_len);
+	return (str);
 }
